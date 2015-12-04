6b95e0df61e5a92dbcb0ff654ed602a9af19936f
Jaime Vasquez <rodrigoxv@gmail.com>
2015-12-04 01:47:34 -0300

    Google Maps

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,18 +9,40 @@ angular.module('starter.controllers', [])
 
   $scope.info = "Hola";
 
-  $scope.login = function(){
-    if (!window.cordova) { 
-      facebookConnectPlugin.browserInit(APP-ID); 
-    } 
-    facebookConnectPlugin.login([], 
-      function (response) { 
-        $scope.login = (JSON.stringify(response)) 
-    }, function (response) 
-    { 
-      $scope.login = (JSON.stringify(response)) 
-    });//fbLoginSuccess, fbLoginError);
-  }
+  // Define a div tag with id="map_canvas"
+  var mapDiv = document.getElementById("map_canvas");
+
+  // Initialize the map plugin
+  var map = plugin.google.maps.Map.getMap(mapDiv);
+
+  // You have to wait the MAP_READY event.
+  map.on(plugin.google.maps.event.MAP_READY, onMapInit);
+
+  function onMapInit(map) {
+    $scope.info = "Mapa Cargado";
+
+    map.getMyLocation(onSuccess, onError); 
+  } 
+
+  var onSuccess = function(location) {
+    var msg = ["Current your location:\n",
+      "latitude:" + location.latLng.lat,
+      "longitude:" + location.latLng.lng,
+      "speed:" + location.speed,
+      "time:" + location.time,
+      "bearing:" + location.bearing].join("\n");
+
+    map.addMarker({
+      'position': location.latLng,
+      'title': msg
+    }, function(marker) {
+      marker.showInfoWindow();
+    });
+  };
+
+  var onError = function(msg) {
+    $scope.info = "error: " + msg;
+  };
 
 });
 
