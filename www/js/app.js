e093f0b139b3119951ba90f1a30fa46bead9c0cd
Jaime Vasquez <rodrigoxv@gmail.com>
2015-12-14 14:36:54 -0300

    cambio var top

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,9 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
+
+var datareg = {};
+
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
@@ -19,8 +22,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
-
-    var datareg = {};
 
     var push = PushNotification.init({
         android: {
