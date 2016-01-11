6147696437894c24fec37c1cb94c0ceb1134dac3
Jaime Vasquez <rodrigoxv@gmail.com>
2016-01-11 12:48:50 -0300

    Test compartir

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,6 +8,17 @@ angular.module('starter.controllers', [])
 .controller('AppCtrlInicio', function($scope) {
 
   $scope.info = "Hola";
+  
+  $scope.compartir = function(){
+    
+    /*
+    text: Text to share, i.e. "Incredible plugin"
+    title: Title of popup, i.e. "Share this quote" (android only, default: "Share")
+    mimetype: Mimetype, i.e. "image/jpeg" (android only, default: "plain/text")
+    */
+    
+    navigator.share("Visita 1G http://onegorilla.cl","Visítanos","plain/text");
+  }
 
 });
 
