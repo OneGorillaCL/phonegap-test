6880646ada3d2beacc21adb2364934ecacca8daa
Jaime Vasquez <rodrigoxv@gmail.com>
2016-01-21 16:09:51 -0300

    Local notification

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,5 +9,23 @@ angular.module('starter.controllers', [])
 
   $scope.info = "Hola";
 
+
+  $scope.agregarNotifiacion = function(){
+  	window.plugins.notification.local.schedule({
+        id: 10,
+        title: "OneGorilla!",
+        text: "Todos somos Gorilla",
+        at: _10_sec_from_now
+    });
+	$scope.info = "Notificacion agregada _10_sec_from_now";
+
+  }
+
+  $scope.eliminarNotificacion = function(){
+  	window.plugins.notification.local.cancel(10, function () {
+	     $scope.info = "Notificacion eliminada";
+	}, scope);
+  }
+
 });
 
