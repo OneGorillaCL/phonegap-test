96c0e847ada5de6f7a6d27e64d5a5ecd5b48c642
Jaime Vasquez <rodrigoxv@gmail.com>
2016-01-22 00:07:05 -0300

    Test streamingMedia

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,5 +9,23 @@ angular.module('starter.controllers', [])
 
   $scope.info = "Hola";
 
+  $scope.verVideo = function(){
+  	  var videoUrl = "http://iphone-streaming.ustream.tv/ustreamVideo/21859489/streams/live/playlist.m3u8";
+
+	  // Just play a video
+	  window.plugins.streamingMedia.playVideo(videoUrl);
+
+	  // Play a video with callbacks
+	  var options = {
+	    successCallback: function() {
+	      console.log("Video was closed without error.");
+	    },
+	    errorCallback: function(errMsg) {
+	      console.log("Error! " + errMsg);
+	    }
+	  };
+	  window.plugins.streamingMedia.playVideo(videoUrl, options);
+  }
+
 });
 
