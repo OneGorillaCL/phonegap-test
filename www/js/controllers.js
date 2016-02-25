5639189cc88da4646d56dff99dc00478ea97d3a6
Jaime Vasquez <rodrigoxv@gmail.com>
2016-02-25 19:14:26 -0300

    Test camara

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,9 +5,35 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('AppCtrlInicio', function($scope) {
+.controller('AppCtrlInicio', function($scope, $cordovaCamera) {
 
   $scope.info = "Hola";
+
+  $scope.foto = function(){
+
+    var options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 100,
+      targetHeight: 100,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false,
+	  correctOrientation:true
+    };
+
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+      var image = document.getElementById('myImage');
+      image.src = "data:image/jpeg;base64," + imageData;
+    }, function(err) {
+      // error
+      $scope.info = err;
+      $scope.$digest();
+    });
+  	
+  }
 
 });
 
