cc1962d9789c727057401f7460c136633a742f7b
Jaime Vasquez <rodrigoxv@gmail.com>
2016-03-17 21:12:37 -0300

    window.plugins.backgroundGeoLocation

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,7 +7,31 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrlInicio', function($scope) {
 
-  $scope.info = "Hola";
+  	$scope.info = "Hola";
+
+ 	var bgGeo = window.plugins.backgroundGeoLocation;
+
+ 	callbackFn = function(location){
+ 		$scope.info = '[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude;
+ 		$scope.$digest();
+ 	}
+
+ 	failureFn = function(error){
+ 		$scope.info = 'BackgroundGeoLocation error';
+ 		$scope.$digest();
+ 	}
+
+	// BackgroundGeoLocation is highly configurable.
+	bgGeo.configure(callbackFn, failureFn, {
+	    activityType: 'AutomotiveNavigation',
+	    debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+	    stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
+	});
+
+	// Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+	bgGeo.start();
+
+
 
 });
 
