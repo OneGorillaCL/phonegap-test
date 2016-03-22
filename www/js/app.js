8ab0576564af2d7edddcbf8d065ae8f22c17abb0
Jaime Vasquez <rodrigoxv@gmail.com>
2016-03-22 01:45:58 -0300

    Cambio de plugin

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -20,12 +20,41 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       StatusBar.styleDefault();
     }
 
-
-    if (window.cordova) {
-      if (window.BackgroundGeolocation) {
-        BackgroundGeolocationService.configurePlugin(window.BackgroundGeolocation);
-      }
-    }
+    /**
+    * This callback will be executed every time a geolocation is recorded in the background.
+    */
+    var callbackFn = function(location) {
+        $rootScope.info = $rootScope.info + '<br>[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude);
+ 
+        // Do your HTTP request here to POST location to your server. 
+        // jQuery.post(url, JSON.stringify(location)); 
+ 
+        /*
+        IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+        and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+        IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+        */
+        backgroundGeoLocation.finish();
+    };
+ 
+    var failureFn = function(error) {
+        console.log('BackgroundGeoLocation error');
+    };
+ 
+    // BackgroundGeoLocation is highly configurable. See platform specific configuration options 
+    backgroundGeoLocation.configure(callbackFn, failureFn, {
+        desiredAccuracy: 10,
+        stationaryRadius: 20,
+        distanceFilter: 30,
+        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle. 
+        stopOnTerminate: false, // <-- enable this to clear background location settings when the app terminates 
+    });
+ 
+    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app. 
+    backgroundGeoLocation.start();
+ 
+    // If you wish to turn OFF background-tracking, call the #stop method. 
+    // backgroundGeoLocation.stop(); 
 
   });
 })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,30 +7,6 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrlInicio', function($scope) {
 
-  	$scope.info = "Hola";
-
- 	var bgGeo = window.backgroundGeoLocation;
-
- 	callbackFn = function(location){
- 		$scope.info = '[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude;
- 		$scope.$digest();
- 	}
-
- 	failureFn = function(error){
- 		$scope.info = 'BackgroundGeoLocation error';
- 		$scope.$digest();
- 	}
-
-	// BackgroundGeoLocation is highly configurable.
-	bgGeo.configure(callbackFn, failureFn, {
-	    activityType: 'AutomotiveNavigation',
-	    debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
-	    stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
-	});
-
-	// Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
-	bgGeo.start();
-
 
 
 });
