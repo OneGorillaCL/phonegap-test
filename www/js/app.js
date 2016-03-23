8215654ad23a2ca17eccd82d08e0b067d333f3f6
Jaime Vasquez <rodrigoxv@gmail.com>
2016-03-22 22:37:07 -0300

    Fix otra vista

--- www/js/app.js
+++ www/js/app.js
@@ -82,7 +82,17 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'AppCtrlInicio'
       }
     }
+  })
+
+  .state('app.detalle', {
+    url: '/detalle',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/detalle.html'
+      }
+    }
   });
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/home');
 });
