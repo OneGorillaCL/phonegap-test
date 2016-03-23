d0daf4e3b04a14b426b6d627a868c3bc76a31f4d
Jaime Vasquez <rodrigoxv@gmail.com>
2016-03-22 23:06:59 -0300

    $rootScope.bgGeo

--- www/js/app.js
+++ www/js/app.js
@@ -19,47 +19,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
-    $rootScope.info = new Array();
-    $rootScope.info.push("Ejecutando");
 
     // Your app must execute AT LEAST ONE call for the current position via standard Cordova geolocation,
     //  in order to prompt the user for Location permission.
     window.navigator.geolocation.getCurrentPosition(function(location) {
-        $rootScope.info.push('Location from Phonegap');
+        console.log('Location from Phonegap');
     });
 
-    var bgGeo = window.plugins.backgroundGeoLocation;
+    $rootScope.bgGeo = window.plugins.backgroundGeoLocation;
 
-    $rootScope.info.push("bgGeo=OK");
 
-    /**
-    * This callback will be executed every time a geolocation is recorded in the background.
-    */
-    var callbackFn = function(location) {
-        $rootScope.info.push('[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude);
-        // Do your HTTP request here to POST location to your server.
-        //
-        //
-        console.log(location);
-    };
-
-    var failureFn = function(error) {
-        console.log('BackgroundGeoLocation error');
-        console.log(error);
-        $rootScope.info.push(error);
-    }
-
-    // BackgroundGeoLocation is highly configurable.
-    bgGeo.configure(callbackFn, failureFn, {
-        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
-        stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
-    });
-
-    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
-    bgGeo.start();
-
-    // If you wish to turn OFF background-tracking, call the #stop method.
-    // bgGeo.stop()
 
   });
 })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,9 +5,38 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('AppCtrlInicio', function($scope) {
+.controller('AppCtrlInicio', function($scope,$rootScope) {
 
+    
+	$scope.info = new Array();
+    /**
+    * This callback will be executed every time a geolocation is recorded in the background.
+    */
+    var callbackFn = function(location) {
+        $scope.info.push('[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude);
+        // Do your HTTP request here to POST location to your server.
+        //
+        //
+        console.log(location);
+    };
 
+    var failureFn = function(error) {
+        console.log('BackgroundGeoLocation error');
+        console.log(error);
+        $scope.info.push(error);
+    }
+
+    // BackgroundGeoLocation is highly configurable.
+    $rootScope.bgGeo.configure(callbackFn, failureFn, {
+        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+        stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
+    });
+
+    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+    $rootScope.bgGeo.start();
+
+    // If you wish to turn OFF background-tracking, call the #stop method.
+    // bgGeo.stop()
 
 });
 
