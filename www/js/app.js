ad6f52d1eef20b70bad35557c2f9810f4dacf896
Jaime Vasquez <rodrigoxv@gmail.com>
2016-03-22 22:29:00 -0300

    update config

--- www/js/app.js
+++ www/js/app.js
@@ -51,10 +51,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
     // BackgroundGeoLocation is highly configurable.
     bgGeo.configure(callbackFn, failureFn, {
-        desiredAccuracy: 10,
-        stationaryRadius: 20,
-        distanceFilter: 30,
-        activityType: 'AutomotiveNavigation',
         debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
         stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
     });
